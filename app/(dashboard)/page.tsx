9735166331a921c7657ad7ca9cb93f3065ee63ea
Bruno Hirae <brunodeluca117@outlook.com>
2024-11-24 16:30:08 -0300

    feat: add card stats components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { Suspense } from 'react';
+import { getFormStats } from '@/actions/form';
+import { Separator } from '@/components/ui/separator';
+import { CardStatsList } from '@/app/(dashboard)/components/card-stats-list';
+
+export default async function Home() {
+  const stats = await getFormStats();
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='container p-4 mx-auto'>
+      <Suspense fallback={<CardStatsList loading={true} />}>
+        <CardStatsList data={stats} loading={false} />
+      </Suspense>
+
+      <Separator className='my-6' />
+
+      <h2 className='text-4xl font-bold col-span-2'>Your forms</h2>
+
+      <Separator className='my-6' />
     </div>
   );
 }
